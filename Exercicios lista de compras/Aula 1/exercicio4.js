acd22973c0aacf76bbe1d138206364b425ba85de
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2025-04-13 11:53:51 -0300

    exercícios da aula 1 feitos

---
+++ Exercicios lista de compras/Aula 1/exercicio4.js
@@ -0,0 +1,19 @@
+/*Exercício 4) Integração do arquivo JavaScript no arquivo HTML
+
+Até aqui, você praticou o JavaScript apenas pelo navegador, mas não construímos um site diretamente no navegador, e sim utilizamos algumas linguagens como HTML, CSS e JavaScript para implementar a estrutura, os estilos e a lógica respectivamente.
+
+Seu objetivo é criar um arquivo HTML ("index.html") e integrar um arquivo JavaScript ("script.js") através da tag <script>. Em seguida, para garantir que a integração esteja funcionando, você pode exibir a mensagem "Integração realizada com sucesso!" no console do projeto.
+
+Lembre-se que você pode usar o atalho exclamação do Emmet (!) para implementar a estrutura HTML rapidamente.
+
+*/
+
+// criar o arquivo index.html e script.js
+
+// criar a estrutura HTML com o atalho exclamação do Emmet (!)
+
+// adicionar a tag <script> no final do body do arquivo HTML ou usar o defer na tag <script> para garantir que o JavaScript seja carregado após a renderização dos elementos HTMl.
+
+// Estrutura da TAG <script> deverá ser assim:
+// <script src="nome-do-arquivo.js"></script>
+// ou <script src="nome-do-arquivo.js" defer></script>
---
+++ Exercicios lista de compras/Aula 1/exercício5.js
@@ -0,0 +1,17 @@
+/*
+Exercício 5) Manipulação do DOM para capturar valores do arquivo HTML com JavaScript
+
+Agora que você já integrou o arquivo JavaScript ao arquivo HTML, você pode acessar os elementos do DOM (Document Object Model) utilizando o JavaScript para manipular os elementos da página de forma dinâmica.
+
+Portanto, seu objetivo é criar uma tag <h1> com um título qualquer no seu arquivo "index.html" e usar o JavaScript para acessar esse elemento e modificar o texto dele.
+
+Uma dica que pode te ajudar é usar o document.querySelector para acessar o documento e o innerHTML para modificar o texto. */
+
+// Criar o arquivo index.html e script.js
+
+// Criando a tag h1:
+// <h1 class="titulo">Título do Projeto</h1>
+
+// No JavaScript, você pode usar o seguinte código para acessar o elemento e modificar o texto:
+// const titulo = document.querySelector(".titulo");
+// console.log(titulo.innerHTML); // Exibe o texto atual do título no console
