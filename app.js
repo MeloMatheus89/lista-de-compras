945f4a1f3f3b2b703552ad2a2443c9d636aaf9ed
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2025-04-13 11:56:03 -0300

    adicionando o comentários para deixar o código mais legível para o Matheus do futuro.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { verificarListaComprados } from "./js/verificarListaComprados.js";
 // Separador de leitura
 const botaoSalvarItem = document.getElementById("adicionar-botao");
 
+// Adicionando o evento de clique no botão. Sintaxe do evento: elemento.addEventListener("evento", função)
 botaoSalvarItem.addEventListener("click", adicionarItem);
 const listaInfernal = document.getElementById("lista-comprados");
 verificarListaComprados(listaInfernal);
